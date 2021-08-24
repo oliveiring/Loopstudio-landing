3baec5567d8647d4f39efa615c64ffd72588e190
kidsacy <michel-oliveira2013@hotmail.com>
2021-08-23 18:44:59 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-$(document).ready(function(){
- if($(window).width() > 767){
-    $("#img").attr("src" , "/images/desktop/image-night-arcade.jpg")
- }else{
-   $("#img").attr("src" , "/images/mobile/image-deep-earth.jpg")
- }
-})
+
